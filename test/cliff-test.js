a7dac3a75e96b8e103943c38cfcba6f4ec4f9bea
indexzero <charlie.robbins@gmail.com>
2012-01-22 16:42:00 -0800

    [test refactor] Merge test/gh-1-regression-test.js into test/cliff-test.js

--- test/cliff-test.js
+++ test/cliff-test.js
@@ -29,19 +29,50 @@ vows.describe('cliff').addBatch({
       }
     },
     "the arrayLengths() method": {
-      "should respond with a list of the longest elements": function () {
-        var lengths, rows = [
-          ["1a", "2a",  "3a",   "4a"],
-          ["1b", "2bb", "3b",   "4b"],
-          ["1c", "2c",  "3ccc", "4c"],
-          ["1d", "2d",  "3dd",  "4dddd"]
+      "with a set of strings": {
+        "should respond with a list of the longest elements": function () {
+          var lengths, rows = [
+            ["1a", "2a",  "3a",   "4a"],
+            ["1b", "2bb", "3b",   "4b"],
+            ["1c", "2c",  "3ccc", "4c"],
+            ["1d", "2d",  "3dd",  "4dddd"]
+          ];
+
+          lengths = cliff.arrayLengths(rows);
+          assert.equal(lengths[0], 2);
+          assert.equal(lengths[1], 3);
+          assert.equal(lengths[2], 4);
+          assert.equal(lengths[3], 5);
+        }
+      },
+      "with a set of numbers and strings": {
+        "should respond with a list of the longest elements": function () {
+          var lengths, rows = [
+            [11, "2a",  "3a",   "4a"],
+            ["1b", 222, "3b",   "4b"],
+            ["1c", "2c",  3333, "4c"],
+            ["1d", "2d",  "3dd",  44444]
+          ];
+
+          lengths = cliff.arrayLengths(rows);
+          assert.equal(lengths[0], 2);
+          assert.equal(lengths[1], 3);
+          assert.equal(lengths[2], 4);
+          assert.equal(lengths[3], 5);
+        }
+      }
+    },
+    "the stringifyRows() method": {
+      "should calculate padding correctly for numbers": function() {
+        var rows = [
+          ['a', 'b'],
+          [12345, 1]
         ];
         
-        lengths = cliff.arrayLengths(rows);
-        assert.equal(lengths[0], 2);
-        assert.equal(lengths[1], 3);
-        assert.equal(lengths[2], 4);
-        assert.equal(lengths[3], 5);
+        assert.equal(
+          cliff.stringifyRows(rows), 
+          'a     b \n12345 1 '
+        );
       }
     }
   }
